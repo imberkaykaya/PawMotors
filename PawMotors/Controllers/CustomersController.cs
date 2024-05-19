6c28cdd2f57c29ef5424d53c42f7c373ee59477f
Berkay Kaya <berkaykaya479@gmail.com>
2024-05-20 00:39:01 +0300

    last cmmt

--- PawMotors/Controllers/CustomersController.cs
+++ PawMotors/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
 using PawMotors.Entity;
 using PawMotors.ProjeContext;
 
@@ -20,14 +21,23 @@ namespace PawMotors.Controllers
         [HttpPost]
         public IActionResult AddCustomer(Customers customer)
         {
-            context.Add(customer);
-            context.SaveChanges();
-            return RedirectToAction("Index");
+            if (customer.FirstName.Length >= 6 && customer.EMail != "")
+            {
+                context.Add(customer);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+
         }
         public IActionResult DeleteCustomer(int id)
         {
             var value = context.Customers.Where(x => x.Id == id).FirstOrDefault();
             context.Remove(value);
+            context.SaveChanges();
             return RedirectToAction("Index");
         }
         [HttpGet]
@@ -42,6 +52,9 @@ namespace PawMotors.Controllers
             var value = context.Customers.Where(x => x.Id == customer.Id).FirstOrDefault();
             value.FirstName = customer.FirstName;
             value.LastName = customer.LastName;
+            value.BirthDay = customer.BirthDay;
+            value.PhoneNumber = customer.PhoneNumber;
+            value.EMail = customer.EMail;
             context.SaveChanges();
             return RedirectToAction("Index");
         }
